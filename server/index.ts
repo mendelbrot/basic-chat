1edb1de76f240f79c27534b4ee1bc71cdea97c12
Gregory Maxedon <gregory.maxedon@gmail.com>
2023-10-07 21:52:41 -0700

    testing

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,14 @@
 import express, { Express, Request, Response } from "express";
-const port = 8000;
 
+const port = 8000;
 const app: Express = express();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("HELLO FROM EXPRESS + TS!!!!");
 });
 
-app.get("/hi", (req: Request, res: Response) => {
-  res.send("BY nogoing");
-});
-
 app.listen(port, () => {
   console.log(`now listening on port ${port}`);
 });
+
+export default app;
---
+++ server/test/api.test.ts
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+import supertest from "supertest";
+import app from "../index";
+
+describe("A simple API endpoint", () => {
+  it("should return a specific response from an endpoint", async () => {
+    const res = await supertest(app).get("/");
+    expect(res.statusCode).to.equal(200);
+    expect(res.text).to.equal("HELLO FROM EXPRESS + TS!!!!");
+  });
+});
