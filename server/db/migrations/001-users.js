d6a93724024d11af4c74d899560286b0163580bd
Gregory Maxedon <gregory.maxedon@gmail.com>
2023-10-14 14:03:50 -0700

    migrations and seeders

--- server/db/migrations/001-users.js
+++ server/db/migrations/001-users.js
@@ -1,24 +1,34 @@
-'use strict';
+"use strict";
+const { QueryTypes } = require("sequelize");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.sequelize.query(
       `CREATE TABLE users (
         id SERIAL PRIMARY KEY,
         username VARCHAR(255) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL,
+        password VARCHAR(255),
         created_at TIMESTAMPTZ NOT NULL,
         updated_at TIMESTAMPTZ NOT NULL
-      );`
+      );`,
+      {
+        type: QueryTypes.CREATE,
+      }
     );
 
     await queryInterface.sequelize.query(
-      `CREATE INDEX idx_username ON users (username);`
+      `CREATE INDEX idx_username ON users (username);`,
+      {
+        type: QueryTypes.CREATE,
+      }
     );
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.sequelize.query(`DROP TABLE users;`);
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`DROP TABLE users;`),
+      {
+        type: QueryTypes.DROP,
+      };
+  },
 };
--- server/db/seeders/001-seeder.js
+++ server/db/seeders/001-seeder.js
@@ -1,19 +1,57 @@
-'use strict';
+"use strict";
+const { QueryTypes } = require("sequelize");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.sequelize.query(
+  async up(queryInterface, Sequelize) {
+    const result1 = await queryInterface.sequelize.query(
       `INSERT INTO users (username, password, created_at, updated_at)
-      VALUES ('user1', 'password', NOW(), NOW());`
+      VALUES ('user1', 'password', NOW(), NOW())
+      RETURNING id;`,
+      {
+        type: QueryTypes.INSERT,
+      }
+    );
+
+    const user1 = result1[0][0].id;
+
+    const result2 = await queryInterface.sequelize.query(
+      `INSERT INTO users (username, password, created_at, updated_at)
+      VALUES ('user2', 'password', NOW(), NOW())
+      RETURNING id;`,
+      {
+        type: QueryTypes.INSERT,
+      }
+    );
+
+    const user2 = result2[0][0].id;
+
+    await queryInterface.sequelize.query(
+      `INSERT INTO messages (content, user_id, created_at, updated_at)
+      VALUES ('hello', :user1, NOW(), NOW());`,
+      {
+        replacements: { user1: user1 },
+        type: QueryTypes.SELECT,
+      }
+    );
+
+    await queryInterface.sequelize.query(
+      `INSERT INTO messages (content, user_id, created_at, updated_at)
+      VALUES ('hi!', :user2, NOW(), NOW());`,
+      {
+        replacements: { user2: user2 },
+        type: QueryTypes.SELECT,
+      }
     );
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.sequelize.query(
-      `DELETE FROM users
-      WHERE username = 'user1';
-      `
-    );
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`DELETE FROM messages`, {
+      type: QueryTypes.DELETE,
+    });
+
+    await queryInterface.sequelize.query(`DELETE FROM users`, {
+      type: QueryTypes.DELETE,
+    });
+  },
 };
