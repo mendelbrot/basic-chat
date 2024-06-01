5c57449fe6a5b6e38387803716599f829a0bf325
Gregory Maxedon <gregory.maxedon@gmail.com>
2024-05-31 19:38:54 -0700

    web: reversed styles for inbound and outbound messages

--- web/src/features/chat/message-list-item.tsx
+++ web/src/features/chat/message-list-item.tsx
@@ -5,10 +5,10 @@ type MessageListItemProps = {
   outbound: boolean;
 };
 
-const outboundStyle =
+const inboundStyle =
   "inline-block bg-blue-600 rounded-full p-2 px-6 text-white";
 
-const inboundStyle =
+const outboundStyle =
   "inline-block bg-gray-300 rounded-full p-2 px-6 text-gray-700";
 
 function MessageListItem({ message, outbound }: MessageListItemProps) {
