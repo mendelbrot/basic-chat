8e63aa1e99bd54a93cccb0ea699075ddddfcc554
Gregory Maxedon <gregory.maxedon@gmail.com>
2024-06-01 11:28:56 -0700

    starting StickerSmash tutorial

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
